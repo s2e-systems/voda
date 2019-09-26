86eb3a2464a8fb320a0082f3aed07e5274bb41d2
Stefan Kimmer <skimmer@s2e-systems.com>
2019-09-26 09:42:21 +0200

    Add googletest for unit tests

---
+++ projects/UnitTests/automatictests.cpp
@@ -0,0 +1,12 @@
+#include <gtest/gtest.h>
+
+TEST(VoDATest, DISABLED_VoDATestSub)
+{
+
+}
+
+int main(int argc, char **argv)
+{
+	::testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
+}
