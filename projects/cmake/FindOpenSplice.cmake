3383d154c0bdcd9ecf25fa4f3ff2ee7ed707d640
Stefan Kimmer <skimmer@s2e-systems.com>
2019-11-26 17:09:27 +0100

    Add NO_SONMAE handling to FindOPenSplice cmake for cross compilation support

--- projects/cmake/FindOpenSplice.cmake
+++ projects/cmake/FindOpenSplice.cmake
@@ -15,6 +15,12 @@ find_library(OpenSplice_dcpssacpp_LIBRARY_DEBUG  NAMES dcpssacppd  PATHS ${OpenS
 find_library(OpenSplice_dcpsisocpp2_LIBRARY  NAMES dcpsisocpp2  PATHS ${OpenSplice_ROOT_SEARCH_PATHS} PATH_SUFFIXES lib)
 find_library(OpenSplice_dcpsisocpp2_LIBRARY_DEBUG  NAMES dcpsisocpp2d  PATHS ${OpenSplice_ROOT_SEARCH_PATHS} PATH_SUFFIXES lib)
 
+find_library(OpenSplice_ddskernel_IMPLIB  NAMES ddskernel.lib  PATHS ${OpenSplice_ROOT_SEARCH_PATHS} PATH_SUFFIXES lib)
+find_library(OpenSplice_dcpssacpp_IMPLIB  NAMES dcpssacpp.lib  PATHS ${OpenSplice_ROOT_SEARCH_PATHS} PATH_SUFFIXES lib)
+find_library(OpenSplice_dcpssacpp_IMPLIB_DEBUG  NAMES dcpssacppd.lib  PATHS ${OpenSplice_ROOT_SEARCH_PATHS} PATH_SUFFIXES lib)
+find_library(OpenSplice_dcpsisocpp2_IMPLIB  NAMES dcpsisocpp2.lib  PATHS ${OpenSplice_ROOT_SEARCH_PATHS} PATH_SUFFIXES lib)
+find_library(OpenSplice_dcpsisocpp2_IMPLIB_DEBUG  NAMES dcpsisocpp2d.lib  PATHS ${OpenSplice_ROOT_SEARCH_PATHS} PATH_SUFFIXES lib)
+
 # Find the idlpp executable
 find_file(OpenSplice_IDLPP_PATH
 	NAMES idlpp idlpp.exe
@@ -24,34 +30,44 @@ find_file(OpenSplice_IDLPP_PATH
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(OpenSplice
-	REQUIRED_VARS OpenSplice_ddskernel_LIBRARY 
+	REQUIRED_VARS OpenSplice_ddskernel_LIBRARY
 	HANDLE_COMPONENTS
 	)
 
 if(OpenSplice_FOUND)
-	add_library(OpenSplice::ddskernel UNKNOWN IMPORTED)
-	set_target_properties(OpenSplice::ddskernel PROPERTIES 
-		IMPORTED_LOCATION ${OpenSplice_ddskernel_LIBRARY} 
-		IMPORTED_LOCATION_DEBUG ${OpenSplice_ddskernel_LIBRARY} 
-		IMPORTED_LOCATION_RELEASE ${OpenSplice_ddskernel_LIBRARY} 
+	add_library(OpenSplice::ddskernel SHARED IMPORTED)
+	set_target_properties(OpenSplice::ddskernel PROPERTIES
+		IMPORTED_LOCATION ${OpenSplice_ddskernel_LIBRARY}
+		IMPORTED_LOCATION_DEBUG ${OpenSplice_ddskernel_LIBRARY}
+		IMPORTED_LOCATION_RELEASE ${OpenSplice_ddskernel_LIBRARY}
 		INTERFACE_INCLUDE_DIRECTORIES "${OpenSplice_INCLUDE_DIR_BASE};${OpenSplice_INCLUDE_DIR_BASE}/sys"
+		IMPORTED_IMPLIB ${OpenSplice_ddskernel_IMPLIB}
+		IMPORTED_NO_SONAME TRUE
 	)
-	
-	add_library(OpenSplice::dcpssacpp UNKNOWN IMPORTED)
-	set_target_properties(OpenSplice::dcpssacpp PROPERTIES 
+
+	add_library(OpenSplice::dcpssacpp SHARED IMPORTED)
+	set_target_properties(OpenSplice::dcpssacpp PROPERTIES
 		IMPORTED_LOCATION ${OpenSplice_dcpssacpp_LIBRARY}
 		IMPORTED_LOCATION_DEBUG ${OpenSplice_dcpssacpp_LIBRARY_DEBUG}
 		INTERFACE_INCLUDE_DIRECTORIES "${OpenSplice_INCLUDE_DIR_BASE}/dcps/C++/SACPP"
+		IMPORTED_IMPLIB ${OpenSplice_dcpssacpp_IMPLIB}
+		IMPORTED_IMPLIB_DEBUG ${OpenSplice_dcpssacpp_IMPLIB_DEBUG}
+		IMPORTED_NO_SONAME TRUE
 	)
-	
-	add_library(OpenSplice::dcpsisocpp2 UNKNOWN IMPORTED)
+
+	add_library(OpenSplice::dcpsisocpp2 SHARED IMPORTED)
 	set_target_properties(OpenSplice::dcpsisocpp2 PROPERTIES
 		IMPORTED_LOCATION ${OpenSplice_dcpsisocpp2_LIBRARY}
 		IMPORTED_LOCATION_DEBUG ${OpenSplice_dcpsisocpp2_LIBRARY_DEBUG}
 		INTERFACE_INCLUDE_DIRECTORIES "${OpenSplice_INCLUDE_DIR_BASE}/dcps/C++/isocpp2"
+		IMPORTED_IMPLIB ${OpenSplice_dcpsisocpp2_IMPLIB}
+		IMPORTED_IMPLIB_DEBUG ${OpenSplice_dcpsisocpp2_IMPLIB_DEBUG}
+		IMPORTED_NO_SONAME TRUE
 	)
-	
+
 endif()
+
+
 
 
 
@@ -80,7 +96,7 @@ macro(OpenSplice_IDLCPP_GEN OpenSplice_IDL_FILEPATH OpenSplice_GENERATED_DIR)
 	if(NOT OpenSplice_IDLPP_PATH)
 		message(FATAL_ERROR "OpenSplice_IDLPP_PATH must be found")
 	endif()
-	
+
 	file(MAKE_DIRECTORY ${OpenSplice_GENERATED_DIR})
 
 	# Get abulute dir since the marking of the generate does not
@@ -89,7 +105,7 @@ macro(OpenSplice_IDLCPP_GEN OpenSplice_IDL_FILEPATH OpenSplice_GENERATED_DIR)
 	# Note the following should be NAME_WLE instead of NAME_WE, but NAME_WLE was introduced only in CMake version 3.14
 	# As such idl filenames including multiple dots in the filename will fail here
 	get_filename_component(OpenSplice_IDL_FILEBASENAME  ${OpenSplice_IDL_FILEPATH} NAME_WE)
-	
+
 	set(OpenSplice_GENERATED_SOURCES_I
 		${OpenSplice_GENERATED_DIR}/${OpenSplice_IDL_FILEBASENAME}.cpp
 		${OpenSplice_GENERATED_DIR}/${OpenSplice_IDL_FILEBASENAME}SplDcps.cpp
