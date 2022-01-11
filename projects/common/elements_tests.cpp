11763b4c927c2c37ff2a459b9457aca79999598d
Stefan Kimmer <skimmer@s2e-systems.com>
2022-01-11 21:59:08 +0100

    remove gst_init from elements_test

--- projects/common/elements_tests.cpp
+++ projects/common/elements_tests.cpp
@@ -3,19 +3,9 @@
 
 TEST(ElementSelectionTest, Selection)
 {
-	GError* gerr = nullptr;
-	auto gret = gst_init_check(nullptr, nullptr, &gerr);
-	if (!gret)
-	{
-		const std::string err = "Could not initialize GStreamer: " + std::string(gerr->message);
-		g_error_free(gerr);
-		FAIL() << err;
-	}
 	ElementSelection e{{"videotestsrc", "noneexistent", "fakesink"}, "name"};
 	EXPECT_EQ(e.elementName(), "videotestsrc");
 
 	EXPECT_THROW((ElementSelection{{"noneexistent"}, "name"}), std::runtime_error);
-
-	gst_deinit();
 }
 
