bfb09f649b03e02dcf464c08210c9e4c0487b9be
Stefan Kimmer <skimmer@s2e-systems.com>
2022-03-21 21:18:01 +0100

    replace qt commandline parser by glib's

--- projects/publisher/main.cpp
+++ projects/publisher/main.cpp
@@ -17,6 +17,8 @@
 #include <QDebug>
 #include <stdexcept>
 #include <string>
+#include <glib.h>
+#include <stdexcept>
 
 #include "videoddspublisher.h"
 #include "videowidgetpaintergst.h"
@@ -26,30 +28,52 @@
 #include "VideoDDS.hpp"
 using namespace org::eclipse::cyclonedds;
 
+
+
+
+
+
+
 int main(int argc, char *argv[])
 {
-	QApplication application(argc, argv);
-	application.setApplicationName("Video DDS Publisher");
-	qDebug() << "This is" << application.applicationName();
-
-	QCommandLineParser parser;
-	parser.setApplicationDescription("VideoDDSpublisher");
-	parser.addHelpOption();
-	parser.addVersionOption();
-	QCommandLineOption useTestSrcOption("testsrc", "Use test src, instead of autovideosrc.");
-	parser.addOption(useTestSrcOption);
-	QCommandLineOption useOmxOption("omx", "Use omx as the encoder.");
-	parser.addOption(useOmxOption);
-	QCommandLineOption useFixedCapsOption("fixed", "Use fixed capabilities for the camera source.");
-	parser.addOption(useFixedCapsOption);
-	QCommandLineOption strengthOption("strength", "Set DDS OwnershipStrength (The higher the number the more stregnth).", "[0 1500]", "1000");
-	parser.addOption(strengthOption);
-	parser.process(application);
-
-	const std::string topicName = "VideoStream";
-
 	try
 	{
+		GError* error = nullptr;
+		GOptionContext* context;
+
+		gboolean use_testsrc = FALSE;
+		gboolean use_omx = FALSE;
+		gboolean use_fixed = FALSE;
+		gint strength = 1000;
+
+		static GOptionEntry entries[] =
+		{
+			{"testsrc", 't', 0, G_OPTION_ARG_NONE, &use_testsrc, "Use test src instead of camera", nullptr},
+			{"omx", 'o', 0, G_OPTION_ARG_NONE, &use_omx, "Use omx as the encoder", nullptr},
+			{"fixed", 'f', 0, G_OPTION_ARG_NONE, &use_fixed, "Use fixed capabilities for the camera source", nullptr},
+			{"strength", 's', 0, G_OPTION_ARG_INT, &strength, "DDS ownership strength", "S"},
+			{nullptr}
+		};
+
+		if (!gst_init_check(&argc, &argv, &error))
+		{
+			const std::string error_message = "Could not initialize GStreamer: " + std::string{error->message};
+			g_error_free(error);
+			throw std::runtime_error{error_message};
+		}
+		context = g_option_context_new("[APPLICATION OPTIONS]");
+		g_option_context_add_main_entries(context, entries, nullptr);
+		g_option_context_add_group(context, gst_init_get_option_group());
+		if (!g_option_context_parse(context, &argc, &argv, &error))
+		{
+			const auto error_message = std::string{error->message};
+			g_error_free(error);
+			throw std::runtime_error{error_message};
+		}
+		g_option_context_free(context);
+
+		const std::string topicName = "VideoStream";
+
 		// Create a domain participant using the default ID configured on the XML file
 		dds::domain::DomainParticipant dp(domain::default_id());
 		dds::topic::qos::TopicQos topicQos = dp.default_topic_qos();
@@ -60,17 +84,19 @@ int main(int argc, char *argv[])
 		// The exclusive ownership allows the use of the ownership strength to define which video source is used.
 		// The liveliness topic determines how to long to wait until the source with lower strength is used
 		// when messages are not received from the source with higher ownership strength.
-		dds::pub::qos::DataWriterQos dwqos = topic.qos();
-		dwqos << dds::core::policy::OwnershipStrength(parser.value(strengthOption).toInt());
-		dwqos << dds::core::policy::WriterDataLifecycle::AutoDisposeUnregisteredInstances();
-		dwqos << dds::core::policy::Ownership::Exclusive();
-		dwqos << dds::core::policy::Liveliness::ManualByTopic(dds::core::Duration::from_millisecs(1000));
+		dds::pub::qos::DataWriterQos dataWriterQos = topic.qos();
+		dataWriterQos << dds::core::policy::OwnershipStrength(strength);
+		dataWriterQos << dds::core::policy::WriterDataLifecycle::AutoDisposeUnregisteredInstances();
+		dataWriterQos << dds::core::policy::Ownership::Exclusive();
+		dataWriterQos << dds::core::policy::Liveliness::ManualByTopic(dds::core::Duration::from_millisecs(1000));
 
-		auto dataWriter = dds::pub::DataWriter<S2E::Video>(pub, topic, dwqos);
+		auto dataWriter = dds::pub::DataWriter<S2E::Video>(pub, topic, dataWriterQos);
 
-		QtGStreamer::init(&argc, &argv, 3 /*log level*/);
+		VideoDDSpublisher publisher{dataWriter, bool(use_testsrc), bool(use_omx), bool(use_fixed)};
 
-		VideoDDSpublisher publisher{dataWriter, parser.isSet(useTestSrcOption), parser.isSet(useOmxOption), parser.isSet(useFixedCapsOption)};
+
+		QApplication application(argc, argv);
+		application.setApplicationName("Video DDS Publisher");
 
 		VideoWidgetPainterGst widget(publisher.appsink());
 		widget.show();
@@ -79,30 +105,30 @@ int main(int argc, char *argv[])
 	}
 	catch (const dds::core::OutOfResourcesError& e)
 	{
-		qFatal("DDS OutOfResourcesError: %s", e.what());
+		std::cerr << "DDS OutOfResourcesError: " << e.what();
 	}
 	catch (const dds::core::InvalidArgumentError& e)
 	{
-		qFatal("DDS InvalidArgumentError: %s", e.what());
+		std::cerr << "DDS InvalidArgumentError: " << e.what();
 	}
 	catch (const dds::core::NullReferenceError& e)
 	{
-		qFatal("DDS NullReferenceError: %s", e.what());
+		std::cerr << "DDS NullReferenceError: " << e.what();
 	}
 	catch (const dds::core::Error& e)
 	{
-		qFatal("DDS Error: %s", e.what());
+		std::cerr << "DDS Error: " << e.what();
 	}
 	catch (const std::range_error& e)
 	{
-		qFatal("range_error: %s", e.what());
+		std::cerr << "range_error: " << e.what();
 	}
 	catch (const std::runtime_error& e)
 	{
-		qFatal("runtime_error: %s", e.what());
+		std::cerr << "runtime_error: " << e.what();
 	}
 	catch (...)
 	{
-		qFatal("Initialization failed with unhandled exeption");
+		std::cerr << "Initialization failed with unhandled exception";
 	}
 }
