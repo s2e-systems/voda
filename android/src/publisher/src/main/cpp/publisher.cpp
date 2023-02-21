544095d810287af5311be02f05fb76e5131a868f
Stefan Kimmer <skimmer@s2e-systems.com>
2023-02-21 22:34:49 +0100

    remove GST_DEBUG category

--- android/src/publisher/src/main/cpp/publisher.cpp
+++ android/src/publisher/src/main/cpp/publisher.cpp
@@ -12,11 +12,6 @@
 #include <gst/app/gstappsink.h>
 #include <pthread.h>
 
-
-GST_DEBUG_CATEGORY_STATIC(debug_category);
-#define GST_CAT_DEFAULT debug_category
-
-
 typedef struct _CustomData
 {
     JavaVM* java_vm;
@@ -24,8 +19,6 @@ typedef struct _CustomData
     jobject app;
     GstElement* pipeline;
     GMainLoop* main_loop;
-    GstElement* video_sink;
-    ANativeWindow* native_window;
 } CustomData;
 
 
@@ -74,10 +67,10 @@ static void state_changed_cb(GstBus *bus, GstMessage *message, CustomData *data)
     GstState pending_state;
     gst_message_parse_state_changed(message, &old_state, &new_state, &pending_state);
     // Only show messages coming from the pipeline, not its children
-    if (GST_MESSAGE_SRC(message) == GST_OBJECT(data->pipeline)) {
+    if (GST_MESSAGE_SRC(message) == GST_OBJECT(data->pipeline))
+    {
         JNIEnv *const jni_env = get_jni_env_from_java_vm(data->java_vm);
-        gchar *const message = g_strdup_printf("State changed to %s",
-                                               gst_element_state_get_name(new_state));
+        gchar *const message = g_strdup_printf("State changed to %s", gst_element_state_get_name(new_state));
         __android_log_print(ANDROID_LOG_INFO, "MyGStreamer", "%s", message);
         set_ui_message(message, jni_env, data->app);
         g_free(message);
@@ -117,13 +110,11 @@ static GstFlowReturn pullSampleAndSendViaDDS(GstAppSink* appSink, gpointer userD
     auto dataWriter = reinterpret_cast<dds::pub::DataWriter<S2E::Video>*>(userData);
     if (dataWriter == nullptr || dataWriter->is_nil())
     {
-        GST_ERROR("DataWriter not valid");
+        __android_log_print(ANDROID_LOG_ERROR, "DDS", "DataWriter not valid");
         return GST_FLOW_ERROR;
     }
 
     const int userid = 0;
-    // Count the samples that have arrived. Used also to define the
-    // DDS msg number later
     static int frameNum = 0;
     frameNum++;
 
@@ -155,21 +146,17 @@ static GstFlowReturn pullSampleAndSendViaDDS(GstAppSink* appSink, gpointer userD
 
 
 /* Main method for the native code. This is executed on its own thread. */
-static void*
-app_function(void* userdata)
+static void* app_function(void* userdata)
 {
-    __android_log_print(ANDROID_LOG_INFO, "MyGStreamer", "app_function");
-
     CustomData* data = (CustomData*)userdata;
 
     JNIEnv* env;
     if (data->java_vm->AttachCurrentThread(&env, nullptr) != JNI_OK) {
-        GST_ERROR("Failed to attach current thread");
+        __android_log_print(ANDROID_LOG_ERROR, "MyGStreamer", "Failed to attach current thread");
         return NULL;
     }
     GMainContext* context = g_main_context_new();
     g_main_context_push_thread_default(context);
-
 
     GstBus* bus = gst_element_get_bus(data->pipeline);
     GSource* bus_source = gst_bus_create_watch(bus);
@@ -184,32 +171,19 @@ app_function(void* userdata)
     g_signal_connect(G_OBJECT(bus), "message::state-changed", (GCallback)state_changed_cb, data);
     gst_object_unref(bus);
 
-    set_ui_message("Entering main loop", env, data->app);
-    __android_log_print(ANDROID_LOG_INFO, "MyGStreamer", "Entering main loop");
     data->main_loop = g_main_loop_new(context, FALSE);
-
-//    if (data->native_window) {
-//        __android_log_print(ANDROID_LOG_INFO, "MyGStreamer", "gst_video_overlay_set_window_handle in app_function");
-//        gst_video_overlay_set_window_handle(GST_VIDEO_OVERLAY(data->video_sink),
-//                                        reinterpret_cast<guintptr>(data->native_window));
-//    }
-    __android_log_print(ANDROID_LOG_INFO, "MyGStreamer", "gst_element_set_state GST_STATE_PLAYING in app_function");
     gst_element_set_state(data->pipeline, GST_STATE_PLAYING);
-
     g_main_loop_run(data->main_loop);
-    GST_DEBUG("Exited main loop");
+
     g_main_loop_unref(data->main_loop);
     data->main_loop = NULL;
-
-    /* Free resources */
     g_main_context_pop_thread_default(context);
     g_main_context_unref(context);
     gst_element_set_state(data->pipeline, GST_STATE_NULL);
-//    gst_object_unref(data->video_sink);
     gst_object_unref(data->pipeline);
 
     if (data->java_vm->DetachCurrentThread() != JNI_OK) {
-        GST_ERROR("Failed to detach current thread");
+        __android_log_print(ANDROID_LOG_ERROR, "MyGStreamer", "Failed to detach current thread");
         return NULL;
     }
     return NULL;
@@ -219,56 +193,42 @@ app_function(void* userdata)
  * Java Bindings
  */
 
-extern "C" JNIEXPORT void JNICALL
-nativeCustomDataInit(JNIEnv * env, jobject thiz) {
+extern "C" JNIEXPORT void JNICALL nativeCustomDataInit(JNIEnv * env, jobject thiz) {
     CustomData* data = g_new0(CustomData, 1);
-    jfieldID custom_data_field_id =
-            env->GetFieldID(env->GetObjectClass(thiz), "native_custom_data", "J");
+    jfieldID custom_data_field_id = env->GetFieldID(env->GetObjectClass(thiz), "native_custom_data", "J");
     env->SetLongField(thiz, custom_data_field_id, (jlong)data);
 }
 
- /* Instruct the native code to create its internal data structure, pipeline and thread */
-extern "C" JNIEXPORT long JNICALL
-nativeLibInit(JNIEnv * env, jobject thiz)
+extern "C" JNIEXPORT long JNICALL nativeLibInit(JNIEnv * env, jobject thiz)
 {
     __android_log_print(ANDROID_LOG_INFO, "MyGStreamer", "nativeLibInit");
-    jfieldID custom_data_field_id =
-            env->GetFieldID(env->GetObjectClass(thiz), "native_custom_data", "J");
+    jfieldID custom_data_field_id = env->GetFieldID(env->GetObjectClass(thiz), "native_custom_data", "J");
     CustomData* data = (CustomData *)env->GetLongField(thiz, custom_data_field_id);
-
-//    GST_DEBUG_CATEGORY_INIT(debug_category, "MyGstreamerBuild", 0,
-//        "Android MyGstreamerBuild");
-//    gst_debug_set_threshold_for_name("MyGstreamerBuild", GST_LEVEL_DEBUG);
-//    GST_DEBUG("Created CustomData at %p", data);
-
     JavaVM* java_vm;
     env->GetJavaVM(&java_vm);
     data->java_vm = java_vm;
-
     data->app = env->NewGlobalRef(thiz);
     __android_log_print(ANDROID_LOG_INFO, "MyGStreamer", "Created GlobalRef for app object at %p for %p", data->app, thiz);
-
     GError* error = NULL;
-    data->pipeline =
-            gst_parse_launch("ahcsrc device=1 ! video/x-raw,format=NV21 ! videoconvert ! tee name=t ! queue leaky=2 ! autovideosink  t. ! queue leaky=2 ! videoconvert ! openh264enc ! appsink name=app_sink",
-                             &error);
-    if (error) {
+
+    data->pipeline = gst_parse_launch("ahcsrc device=1 ! video/x-raw,format=NV21 ! videoconvert ! tee name=t ! queue leaky=2 ! autovideosink  t. ! queue leaky=2 ! videoconvert ! openh264enc ! appsink name=app_sink",&error);
+    if (error)
+    {
         __android_log_print(ANDROID_LOG_ERROR, "MyGStreamer", "gst_parse_launch failed");
-        gchar* message =
-                g_strdup_printf("Unable to build pipeline: %s", error->message);
+        gchar* message = g_strdup_printf("Unable to build pipeline: %s", error->message);
         g_clear_error(&error);
         set_ui_message(message, env, data->app);
         g_free(message);
     }
 
     GstElement* app_sink = gst_bin_get_by_name(GST_BIN(data->pipeline), "app_sink");
-    if (!app_sink) {
+    if (!app_sink)
+    {
         __android_log_print(ANDROID_LOG_ERROR, "MyGStreamer", "Could not retrieve app_sink");
     }
 
     g_object_set(app_sink,
                  "emit-signals", true,
-            //                 "caps", ddsSinkCaps,
                  "max-buffers", 1,
                  "drop", false,
                  "sync", false,
@@ -294,35 +254,26 @@ nativeLibInit(JNIEnv * env, jobject thiz)
         dataWriterQos << dds::core::policy::Liveliness::ManualByTopic(dds::core::Duration::from_millisecs(5000));
         dataWriterQos << dds::core::policy::OwnershipStrength(1000);
         data_writer = new DataWriter<S2E::Video>{ publisher, topic, dataWriterQos };
-    }
-    catch (const dds::core::Exception& e) {
+    } catch (const dds::core::Exception& e)
+    {
         __android_log_print(ANDROID_LOG_ERROR, "DDS", "DDS initializaion failed with: %s", e.what());
     }
 
-    g_signal_connect(app_sink, "new-sample", G_CALLBACK(pullSampleAndSendViaDDS),
-                     reinterpret_cast<gpointer>(data_writer));
-
-    /* Set the pipeline to READY, so it can already accept a window handle, if we have one */
+    g_signal_connect(app_sink, "new-sample", G_CALLBACK(pullSampleAndSendViaDDS), reinterpret_cast<gpointer>(data_writer));
+    // Set the pipeline to READY to receive a video_sink
     gst_element_set_state(data->pipeline, GST_STATE_READY);
-
-    auto video_sink =
-            gst_bin_get_by_interface(GST_BIN(data->pipeline),
-                                     GST_TYPE_VIDEO_OVERLAY);
-    if (!video_sink) {
+    auto video_sink = gst_bin_get_by_interface(GST_BIN(data->pipeline), GST_TYPE_VIDEO_OVERLAY);
+    if (!video_sink)
+    {
         __android_log_print(ANDROID_LOG_ERROR, "MyGStreamer", "Could not retrieve video sink");
     }
-
     pthread_create(&data->gst_app_thread, NULL, &app_function, data);
-
     return reinterpret_cast<jlong>(video_sink);
 }
 
-/* Quit the main loop, remove the native thread and free resources */
-extern "C" JNIEXPORT void JNICALL
-nativeFinalize(JNIEnv * env, jobject thiz)
+extern "C" JNIEXPORT void JNICALL nativeFinalize(JNIEnv * env, jobject thiz)
 {
-    jfieldID custom_data_field_id =
-            env->GetFieldID(env->GetObjectClass(thiz), "native_custom_data", "J");
+    jfieldID custom_data_field_id = env->GetFieldID(env->GetObjectClass(thiz), "native_custom_data", "J");
     CustomData* data = (CustomData *)env->GetLongField(thiz, custom_data_field_id);
     if (!data)
         return;
@@ -331,13 +282,12 @@ nativeFinalize(JNIEnv * env, jobject thiz)
     env->DeleteGlobalRef(data->app);
     g_free(data);
     env->SetLongField(thiz, custom_data_field_id, 0);
+
     delete data_writer;
     delete domain_participant;
 }
 
-
-extern "C" JNIEXPORT void JNICALL
-nativeSurfaceInit(JNIEnv* env, jobject thiz, jobject surface, long video_sink)
+extern "C" JNIEXPORT void JNICALL nativeSurfaceInit(JNIEnv* env, jobject thiz, jobject surface, long video_sink)
 {
     auto native_window = reinterpret_cast<guintptr>(ANativeWindow_fromSurface(env, surface));
     gst_video_overlay_set_window_handle(GST_VIDEO_OVERLAY(video_sink), native_window);
@@ -348,7 +298,6 @@ nativeSurfaceFinalize(JNIEnv * env, jobject thiz, jobject surface)
 {
     ANativeWindow_release(ANativeWindow_fromSurface(env, surface));
 }
-
 
 jint JNI_OnLoad(JavaVM* vm, void* reserved)
 {
