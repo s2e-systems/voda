80aed7c074797412f5d60d4cf09bd1e7212336a8
Stefan Kimmer <skimmer@s2e-systems.com>
2023-09-25 21:50:31 +0200

    split binding

--- android/src/publisher/src/main/cpp/publisher_binding.cpp
+++ android/src/publisher/src/main/cpp/publisher_binding.cpp
@@ -1,7 +1,5 @@
 #include <jni.h>
 #include <android/log.h>
-#include <android/native_window_jni.h>
-#include <gst/video/video.h>
 #include "Publisher.h"
 #include "MainActivityBinding.h"
 
@@ -23,35 +21,25 @@ Java_com_s2e_1systems_MainActivity_nativePublisherInit(JNIEnv *env, jobject thiz
         </Interfaces></General></Domain></CycloneDDS>)", 1);
 
     try {
-        JavaVM* java_vm;
+        JavaVM *java_vm;
         env->GetJavaVM(&java_vm);
-        std::unique_ptr<MainActivityBinding> main_activity_binding{new MainActivityBinding{java_vm, thiz}};
-        NATIVE_PUBLISHER = new Publisher(dds::domain::DomainParticipant{domain::default_id()}, std::move(main_activity_binding));
+        std::unique_ptr<MainActivityBinding> main_activity_binding{
+                new MainActivityBinding{java_vm, thiz}};
+        NATIVE_PUBLISHER = new Publisher(dds::domain::DomainParticipant{domain::default_id()},
+                                         std::move(main_activity_binding));
         return reinterpret_cast<jlong>(NATIVE_PUBLISHER->video_sink());
     } catch (const dds::core::Exception &e) {
-        __android_log_print(ANDROID_LOG_ERROR, "DDS", "DDS initializaion failed with: %s", e.what());
-    } catch (const std::runtime_error &e) {
-        __android_log_print(ANDROID_LOG_ERROR, "NativePublisher", "PublisherInit failed with: %s", e.what());
+        __android_log_print(ANDROID_LOG_ERROR, "NativePublisher",
+                            "DDS initializaion failed with: %s", e.what());
+    } catch (const std::exception &e) {
+        __android_log_print(ANDROID_LOG_ERROR, "NativePublisher", "PublisherInit failed with: %s",
+                            e.what());
     }
     return 0;
 }
 
 extern "C"
 JNIEXPORT void JNICALL
-Java_com_s2e_1systems_SurfaceHolderCallback_nativeSurfaceInit(JNIEnv *env, jobject,
-                                                          jobject surface, jlong video_sink) {
-    auto native_window = reinterpret_cast<guintptr>(ANativeWindow_fromSurface(env, surface));
-    gst_video_overlay_set_window_handle(GST_VIDEO_OVERLAY(video_sink), native_window);
-}
-
-extern "C"
-JNIEXPORT void JNICALL
-Java_com_s2e_1systems_SurfaceHolderCallback_nativeSurfaceFinalize(JNIEnv *env, jobject,
-                                                              jobject surface) {
-    ANativeWindow_release(ANativeWindow_fromSurface(env, surface));
-}
-extern "C"
-JNIEXPORT void JNICALL
-Java_com_s2e_1systems_MainActivity_nativePublisherFinalize(JNIEnv *, jobject ) {
+Java_com_s2e_1systems_MainActivity_nativePublisherFinalize(JNIEnv *, jobject) {
     delete NATIVE_PUBLISHER;
 }
