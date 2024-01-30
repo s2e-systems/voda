638bfc9ddba0fbe9fbdb30cc0ca4cbad84c129ac
Stefan Kimmer <skimmer@s2e-systems.com>
2024-01-30 22:24:19 +0100

    merge into nativeInit

--- android/src/publisher/src/main/cpp/gstreamer_android-1.0.c
+++ android/src/publisher/src/main/cpp/gstreamer_android-1.0.c
@@ -4,12 +4,9 @@
 #include <android/log.h>
 #include <string.h>
 
-
 static jobject _context = NULL;
 static jobject _class_loader = NULL;
 static JavaVM *_java_vm = NULL;
-static GstClockTime _priv_gst_info_start_time;
-
 
 /* Declaration of static plugins */
 GST_PLUGIN_STATIC_DECLARE(androidmedia);
@@ -39,82 +36,6 @@ void gst_init_static_plugins(void)
     GST_PLUGIN_STATIC_REGISTER(videorate);
 }
 
-static void
-gst_debug_logcat(GstDebugCategory *category, GstDebugLevel level,
-                 const gchar *file, const gchar *function, gint line,
-                 GObject *object, GstDebugMessage *message, gpointer unused)
-{
-    GstClockTime elapsed;
-    gint android_log_level;
-    gchar *tag;
-
-    if (level > gst_debug_category_get_threshold(category))
-        return;
-
-    elapsed = GST_CLOCK_DIFF(_priv_gst_info_start_time,
-                             gst_util_get_timestamp());
-
-    switch (level)
-    {
-    case GST_LEVEL_ERROR:
-        android_log_level = ANDROID_LOG_ERROR;
-        break;
-    case GST_LEVEL_WARNING:
-        android_log_level = ANDROID_LOG_WARN;
-        break;
-    case GST_LEVEL_INFO:
-        android_log_level = ANDROID_LOG_INFO;
-        break;
-    case GST_LEVEL_DEBUG:
-        android_log_level = ANDROID_LOG_DEBUG;
-        break;
-    default:
-        android_log_level = ANDROID_LOG_VERBOSE;
-        break;
-    }
-
-    tag = g_strdup_printf("GStreamer+%s",
-                          gst_debug_category_get_name(category));
-
-    if (object)
-    {
-        gchar *obj;
-
-        if (GST_IS_PAD(object) && GST_OBJECT_NAME(object))
-        {
-            obj = g_strdup_printf("<%s:%s>", GST_DEBUG_PAD_NAME(object));
-        }
-        else if (GST_IS_OBJECT(object) && GST_OBJECT_NAME(object))
-        {
-            obj = g_strdup_printf("<%s>", GST_OBJECT_NAME(object));
-        }
-        else if (G_IS_OBJECT(object))
-        {
-            obj = g_strdup_printf("<%s@%p>", G_OBJECT_TYPE_NAME(object), object);
-        }
-        else
-        {
-            obj = g_strdup_printf("<%p>", object);
-        }
-
-        __android_log_print(android_log_level, tag,
-                            "%" GST_TIME_FORMAT " %p %s:%d:%s:%s %s\n",
-                            GST_TIME_ARGS(elapsed), g_thread_self(),
-                            file, line, function, obj, gst_debug_message_get(message));
-
-        g_free(obj);
-    }
-    else
-    {
-        __android_log_print(android_log_level, tag,
-                            "%" GST_TIME_FORMAT " %p %s:%d:%s %s\n",
-                            GST_TIME_ARGS(elapsed), g_thread_self(),
-                            file, line, function, gst_debug_message_get(message));
-    }
-    g_free(tag);
-}
-
-
 jobject
 gst_android_get_application_context(void)
 {
@@ -133,81 +54,23 @@ gst_android_get_java_vm(void)
     return _java_vm;
 }
 
-static gboolean
-init(JNIEnv *env, jobject context)
+
+JNIEXPORT void JNICALL
+Java_org_freedesktop_gstreamer_GStreamer_nativeInit(JNIEnv *env, jobject gstreamer, jobject context)
 {
-    jclass context_cls = NULL;
+    GError *error = NULL;
     jmethodID get_class_loader_id = 0;
 
-    jobject class_loader = NULL;
-
-    context_cls = (*env)->GetObjectClass(env, context);
-    if (!context_cls)
-    {
-        return FALSE;
-    }
-
+    jclass context_cls = (*env)->GetObjectClass(env, context);
     get_class_loader_id = (*env)->GetMethodID(env, context_cls,
                                               "getClassLoader", "()Ljava/lang/ClassLoader;");
-    if ((*env)->ExceptionCheck(env))
-    {
-        (*env)->ExceptionDescribe(env);
-        (*env)->ExceptionClear(env);
-        return FALSE;
-    }
-
-    class_loader = (*env)->CallObjectMethod(env, context, get_class_loader_id);
-    if ((*env)->ExceptionCheck(env))
-    {
-        (*env)->ExceptionDescribe(env);
-        (*env)->ExceptionClear(env);
-        return FALSE;
-    }
-
-    if (_context)
-    {
-        (*env)->DeleteGlobalRef(env, _context);
-    }
+    jobject class_loader = (*env)->CallObjectMethod(env, context, get_class_loader_id);
     _context = (*env)->NewGlobalRef(env, context);
-
-    if (_class_loader)
-    {
-        (*env)->DeleteGlobalRef(env, _class_loader);
-    }
     _class_loader = (*env)->NewGlobalRef(env, class_loader);
 
-    return TRUE;
-}
-
-void gst_android_init(JNIEnv *env, jobject context)
-{
-    gchar *cache_dir;
-    gchar *files_dir;
-    gchar *registry;
-    GError *error = NULL;
-
-    if (!init(env, context))
-    {
-        __android_log_print(ANDROID_LOG_INFO, "GStreamer",
-                            "GStreamer failed to initialize");
-    }
-
-    if (gst_is_initialized())
-    {
-        __android_log_print(ANDROID_LOG_INFO, "GStreamer",
-                            "GStreamer already initialized");
-        return;
-    }
-
-    /* Disable this for releases if performance is important
-     * or increase the threshold to get more information */
     gst_debug_set_active(TRUE);
     gst_debug_set_default_threshold(GST_LEVEL_WARNING);
     gst_debug_remove_log_function(gst_debug_log_default);
-    gst_debug_add_log_function((GstLogFunction)gst_debug_logcat, NULL, NULL);
-
-    /* get time we started for debugging messages */
-    _priv_gst_info_start_time = gst_util_get_timestamp();
 
     if (!gst_init_check(NULL, NULL, &error))
     {
@@ -219,14 +82,6 @@ void gst_android_init(JNIEnv *env, jobject context)
         g_free(message);
         return;
     }
-    __android_log_print(ANDROID_LOG_INFO, "GStreamer",
-                        "GStreamer initialization complete");
-}
-
-JNIEXPORT void JNICALL
-Java_org_freedesktop_gstreamer_GStreamer_nativeInit(JNIEnv *env, jobject gstreamer, jobject context)
-{
-    gst_android_init(env, context);
 }
 
 jint JNI_OnLoad(JavaVM *vm, void *reserved)
