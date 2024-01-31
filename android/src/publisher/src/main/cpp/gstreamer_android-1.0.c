922d4b37fdde275851f45672529884d36849070c
Stefan Kimmer <54408686+stkimmer@users.noreply.github.com>
2024-01-31 20:24:00 +0100

    Update GStreamer and Android SDK (#12)
    * update android sdk and gstreamer
    
    * remove some unnecessary code
    
    * remove get_application_dirs
    
    * merge into nativeInit
    
    * remove some checks
    
    * clean subscriber
    
    * update GStreamer version
    
    * clean gitignore
    
    * update material
    
    * trigger on sub folders
    
    * ignore all android subpaths
    
    * try remove filter
    
    * remove filter entirely
    
    * move keystorePropertiesFile
    
    * only build debug
    
    * build bundle
    
    * update gstreamer in docker
    
    * update sdk in docker

---
+++ android/src/publisher/src/main/cpp/gstreamer_android-1.0.c
@@ -0,0 +1,89 @@
+#include <jni.h>
+#include <gst/gst.h>
+
+static jobject CONTEXT = NULL;
+static jobject CLASS_LOADER = NULL;
+static JavaVM *JAVA_VM = NULL;
+
+/* Declaration of static plugins */
+GST_PLUGIN_STATIC_DECLARE(androidmedia);
+GST_PLUGIN_STATIC_DECLARE(app);
+GST_PLUGIN_STATIC_DECLARE(autodetect);
+GST_PLUGIN_STATIC_DECLARE(coreelements);
+GST_PLUGIN_STATIC_DECLARE(opengl);
+GST_PLUGIN_STATIC_DECLARE(openh264);
+GST_PLUGIN_STATIC_DECLARE(videoconvertscale);
+GST_PLUGIN_STATIC_DECLARE(videofilter);
+GST_PLUGIN_STATIC_DECLARE(videotestsrc);
+GST_PLUGIN_STATIC_DECLARE(videorate);
+
+/* This is called by gst_init() to register static plugins */
+void gst_init_static_plugins(void)
+{
+    GST_PLUGIN_STATIC_REGISTER(androidmedia);
+    GST_PLUGIN_STATIC_REGISTER(app);
+    GST_PLUGIN_STATIC_REGISTER(autodetect);
+    GST_PLUGIN_STATIC_REGISTER(coreelements);
+    GST_PLUGIN_STATIC_REGISTER(opengl);
+    GST_PLUGIN_STATIC_REGISTER(openh264);
+    GST_PLUGIN_STATIC_REGISTER(videoconvertscale);
+    GST_PLUGIN_STATIC_REGISTER(videofilter);
+    GST_PLUGIN_STATIC_REGISTER(videotestsrc);
+    GST_PLUGIN_STATIC_REGISTER(videorate);
+}
+
+/* AMC plugin uses g_module_symbol() to find this symbol */
+jobject
+gst_android_get_application_context(void)
+{
+    return CONTEXT;
+}
+
+/* AMC plugin uses g_module_symbol() to find this symbol */
+jobject
+gst_android_get_application_class_loader(void)
+{
+    return CLASS_LOADER;
+}
+
+/* AMC plugin uses g_module_symbol() to find this symbol */
+JavaVM *
+gst_android_get_java_vm(void)
+{
+    return JAVA_VM;
+}
+
+JNIEXPORT void JNICALL
+Java_org_freedesktop_gstreamer_GStreamer_nativeInit(JNIEnv *env, jobject gstreamer, jobject context)
+{
+    GError *error = NULL;
+    CONTEXT = (*env)->NewGlobalRef(env, context);
+    jclass context_cls = (*env)->GetObjectClass(env, context);
+    jmethodID get_class_loader_id = (*env)->GetMethodID(env, context_cls, "getClassLoader", "()Ljava/lang/ClassLoader;");
+    jobject class_loader = (*env)->CallObjectMethod(env, context, get_class_loader_id);
+
+    CLASS_LOADER = (*env)->NewGlobalRef(env, class_loader);
+
+    if (!gst_init_check(NULL, NULL, &error))
+    {
+        gchar *message = g_strdup_printf("GStreamer initialization failed: %s",
+                                         error && error->message ? error->message : "(no message)");
+        jclass exception_class = (*env)->FindClass(env, "java/lang/Exception");
+        (*env)->ThrowNew(env, exception_class, message);
+        g_free(message);
+    }
+}
+
+jint JNI_OnLoad(JavaVM *vm, void *reserved)
+{
+    JAVA_VM = vm;
+    return JNI_VERSION_1_4;
+}
+
+void JNI_OnUnload(JavaVM *vm, void *reversed)
+{
+    JNIEnv *env = NULL;
+    (*vm)->GetEnv(vm, (void **)&env, JNI_VERSION_1_4);
+    (*env)->DeleteGlobalRef(env, CLASS_LOADER);
+    (*env)->DeleteGlobalRef(env, CONTEXT);
+}
--- android/src/publisher/src/main/cpp/publisher_binding.cpp
+++ android/src/publisher/src/main/cpp/publisher_binding.cpp
@@ -7,39 +7,41 @@ using namespace org::eclipse::cyclonedds;
 
 Publisher *NATIVE_PUBLISHER = nullptr;
 
-jint JNI_OnLoad(JavaVM *, void *) {
-    return JNI_VERSION_1_4;
-}
-
-extern "C"
-JNIEXPORT jlong JNICALL
-Java_com_s2e_1systems_MainActivity_nativePublisherInit(JNIEnv *env, jobject thiz, int orientation) {
+extern "C" JNIEXPORT jlong JNICALL
+Java_com_s2e_1systems_MainActivity_nativePublisherInit(JNIEnv *env, jobject thiz, int orientation)
+{
 
     setenv("CYCLONEDDS_URI", R"(<CycloneDDS><Domain><General><Interfaces>
         <NetworkInterface name="eth1" presence_required="false" />
         <NetworkInterface name="wlan0" presence_required="false" />
-        </Interfaces></General></Domain></CycloneDDS>)", 1);
+        </Interfaces></General></Domain></CycloneDDS>)",
+           1);
 
-    try {
+    try
+    {
         JavaVM *java_vm;
         env->GetJavaVM(&java_vm);
         std::unique_ptr<MainActivityBinding> main_activity_binding{
-                new MainActivityBinding{java_vm, thiz}};
+            new MainActivityBinding{java_vm, thiz}};
         NATIVE_PUBLISHER = new Publisher(dds::domain::DomainParticipant{domain::default_id()},
                                          std::move(main_activity_binding), orientation);
         return reinterpret_cast<jlong>(NATIVE_PUBLISHER->video_sink());
-    } catch (const dds::core::Exception &e) {
+    }
+    catch (const dds::core::Exception &e)
+    {
         __android_log_print(ANDROID_LOG_ERROR, "NativePublisher",
                             "DDS initialization failed with: %s", e.what());
-    } catch (const std::exception &e) {
+    }
+    catch (const std::exception &e)
+    {
         __android_log_print(ANDROID_LOG_ERROR, "NativePublisher", "PublisherInit failed with: %s",
                             e.what());
     }
     return 0;
 }
 
-extern "C"
-JNIEXPORT void JNICALL
-Java_com_s2e_1systems_MainActivity_nativePublisherFinalize(JNIEnv *, jobject) {
+extern "C" JNIEXPORT void JNICALL
+Java_com_s2e_1systems_MainActivity_nativePublisherFinalize(JNIEnv *, jobject)
+{
     delete NATIVE_PUBLISHER;
 }
